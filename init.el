c961e30866fa4bd089d7e2700019bccbab19eb88
Nick Booker <nmb@nickbooker.uk>
2016-07-23 15:45:13 +0100

    Disable startup screen

--- init.el
+++ init.el
@@ -63,7 +63,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen nil)
+ '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
     (magit helm-ls-git helm tagedit-nxml tagedit multiple-cursors)))
